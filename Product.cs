2e21d39e7db3b8ce6869c34a66d2f6e80b957a8c
Coder Club <hi@coderclub.com>
2022-10-28 00:46:10 +0300

    atıldı

---
+++ Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdoNetDemo
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        public int StockAmount { get; set; }
+
+
+
+
+    }
+}
---
+++ ProductDal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Data;
+
+namespace AdoNetDemo
+{
+    public class ProductDal
+    {
+        public DataTable GetAll()
+        {
+            SqlConnection connections = new SqlConnection("server=(localdb)\mssqllocaldb;inittal catalog=ETrade;integrated securutiy=true");
+            if (connections.State==ConnectionState.Closed)
+            {
+                connections.Open();    
+            }
+            SqlCommand command = new SqlCommand("Select * from Products",connections);
+
+            SqlDataReader reader=command.ExecuteReader();
+            DataTable datatable = new DataTable();
+            dataTable.Load(reader);
+
+        }
+    }
+}
